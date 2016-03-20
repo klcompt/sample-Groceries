a9e4d8410984d6f4cb8fc766e361ff76e7dce917
Kevin Compton <klcompt@gmail.com>
2016-03-19 19:58:16 -0500

    KC1

--- app/shared/view-models/user-view-model.js
+++ app/shared/view-models/user-view-model.js
@@ -1,6 +1,7 @@
 var config = require("../../shared/config");
 var fetchModule = require("fetch");
 var Observable = require("data/observable").Observable;
+var validator = require("email-validator");
 
 function User(info) {
     info = info || {};
@@ -23,6 +24,32 @@ function User(info) {
                 "Content-Type": "application/json"
             }
         }).then(handleErrors);
+    };
+
+    viewModel.login = function() {
+      return fetchModule.fetch(config.apiUrl + "oauth/token", {
+        method: "POST",
+        body: JSON.stringify({
+          username: viewModel.get("email"),
+          password: viewModel.get("password"),
+          grant_type: "password"
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(handleErrors)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        config.token = data.Result.access_token;
+      });
+    };
+
+    viewModel.isValidEmail = function() {
+      var email = this.get("email");
+      return validator.validate(email);
     };
 
     return viewModel;
